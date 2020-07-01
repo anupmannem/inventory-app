a7597cf0faf8ed060667fc2065bb37e204b776f1
anupmannem <anupmannem@gmail.com>
2020-07-01 15:07:58 +0530

    states routes setup

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -14,5 +14,19 @@ module.exports = {
     seeds: {
       directory: './db/seeds',
     }
+  },
+  test: {       
+    client: 'pg',
+    connection: {
+      database: process.env.POSTGRES_TEST_DB,
+      user:     process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD
+    },
+    migrations: {
+      directory: './db/migrations',
+    },
+    seeds: {
+      directory: './db/seeds',
+    }
   }
 };
